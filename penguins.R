1fdafb9fca923e33b04580716a7a28310388608d
ziire <irenez@uio.no>
2025-11-14 09:57:14 +0100

    my first plots

---
+++ penguins.R
@@ -0,0 +1,24 @@
+
+#first small script
+library(palmerpenguins)
+library(dplyr)
+library(ggplot2)
+theme_set(theme_minimal())
+
+## Count penguins for each species / island
+num_penguins <- ggplot(penguins, aes(x = island, fill = species)) +
+  geom_bar(alpha = 0.8) +
+  scale_fill_manual(values = c("darkorange","purple","cyan4"), 
+                    guide = FALSE) +
+  theme_minimal() +
+  facet_wrap(~species, ncol = 1) +
+  coord_flip()
+num_penguins
+
+## Scatterplot: penguin flipper length versus body mass
+plot2 <- ggplot(data = penguins, aes(x = flipper_length_mm, y = body_mass_g)) +
+  geom_point(aes(color = species, 
+                 shape = species),
+             size = 2) +
+  scale_color_manual(values = c("darkorange","darkorchid","cyan4")) 
+plot2
